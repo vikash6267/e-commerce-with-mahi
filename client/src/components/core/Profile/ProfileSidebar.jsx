2c6d01c1c6e1504c4a3614e68559feaae7e7e1b0
Rishi <rishimaheshwari040@gmail.com>
2024-05-12 18:38:08 +0530

    sidebar fix

---
+++ client/src/components/core/Profile/ProfileSidebar.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect } from "react";
+import { AiOutlineLogin, AiOutlineMessage } from "react-icons/ai";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { HiOutlineReceiptRefund, HiOutlineShoppingBag } from "react-icons/hi";
+import {
+  MdOutlineAdminPanelSettings,
+  MdOutlinePassword,
+  MdOutlineTrackChanges,
+} from "react-icons/md";
+import { TbAddressBook } from "react-icons/tb";
+import { RxPerson } from "react-icons/rx";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const ProfileSidebar = ({ setActive, active }) => {
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.profile);
+
+  useEffect(() => {
+    console.log(user);
+  }, []);
+
+  const logoutHandler = () => {};
+
+  return (
+    <div className="sm:w-[40px] md:w-[120px]  ">
+      <div className=" bg-white flex flex-col rounded pt-5 pl-2">
+        <MenuItem
+          icon={<RxPerson size={20} />}
+          name="Profile"
+          number={1}
+          setActive={setActive}
+          active={active}
+        />
+        <MenuItem
+          icon={<HiOutlineShoppingBag size={20} />}
+          name="Orders"
+          number={2}
+          setActive={setActive}
+          active={active}
+        />
+        <MenuItem
+          icon={<HiOutlineReceiptRefund size={20} />}
+          name="Refunds"
+          number={3}
+          setActive={setActive}
+          active={active}
+        />
+        <MenuItem
+          icon={<AiOutlineMessage size={20} />}
+          name="Inbox"
+          number={4}
+          setActive={setActive}
+          active={active}
+        />
+        <MenuItem
+          icon={<MdOutlineTrackChanges size={20} />}
+          name="Track Order"
+          number={5}
+          setActive={setActive}
+          active={active}
+        />
+        <MenuItem
+          icon={<RiLockPasswordLine size={20} />}
+          name="Change Password"
+          number={6}
+          setActive={setActive}
+          active={active}
+        />
+        <MenuItem
+          icon={<TbAddressBook size={20} />}
+          name="Address"
+          number={7}
+          setActive={setActive}
+          active={active}
+        />
+        {user && user?.role === "Admin" && (
+          <Link to="/admin/dashboard" onClick={() => setActive(8)}>
+            <MenuItem
+              icon={<MdOutlineAdminPanelSettings size={20} />}
+              name="Admin Dashboard"
+              number={8}
+              setActive={setActive}
+              active={active}
+            />
+          </Link>
+        )}
+        <MenuItem
+          icon={<AiOutlineLogin size={20} />}
+          name="Log out"
+          number={9}
+          setActive={setActive}
+          active={active}
+        />
+      </div>
+    </div>
+  );
+};
+
+const MenuItem = ({ icon, name, number, setActive, active }) => {
+  return (
+    <div
+      className={`flex items-center cursor-pointer w-full mb-8 ${
+        active === number ? "text-red" : ""
+      }`}
+      onClick={() => setActive(number)}
+    >
+      <div className="flex items-center">
+        <div className="">{icon}</div>
+        <span className="pl-3 hidden sm:flex">{name}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileSidebar;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,19 +1,32 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from "react";
+import ProfileContent from "../components/core/Profile/ProfileContent";
+import ProfileSidebar from "../components/core/Profile/ProfileSidebar";
+import { useSelector } from "react-redux";
 
-function Profile() {
-  const{user} = useSelector(state=> state.profile)
+const ProfilePage = () => {
+  const { loading } = useSelector((state) => state.profile);
+  const [active, setActive] = useState(1);
 
-  useEffect(()=>{
-    console.log(user)
-  },[])
   return (
-    <div className=' mt-[65px] w-[80%] mx-auto font '>
-
-
-
+    <div>
+      {loading ? (
+        <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <>
+          <div
+            className={`w 11/12 mx-auto flex sm:gap-3 lg:gap-[100px]  bg-[#f5f5f5] py-10`}
+          >
+            <div className="w-[50px] 800px:w-[335px] sticky 800px:mt-0 mt-[110px]">
+              <ProfileSidebar active={active} setActive={setActive} />
+            </div>
+            <ProfileContent active={active} />
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default ProfilePage;
