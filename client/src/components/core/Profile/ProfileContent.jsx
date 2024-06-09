ce1c822d22280467d2f49286dae84322704be9ee
Rishi <rishimaheshwari040@gmail.com>
2024-06-09 16:42:52 +0530

    update

--- client/src/components/core/Profile/ProfileContent.jsx
+++ client/src/components/core/Profile/ProfileContent.jsx
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
-import {
-  AiOutlineCamera,
-} from "react-icons/ai";
+import React, { useState, useEffect } from "react";
+import { AiOutlineCamera } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
-import styles from "./styles";
-// import { DataGrid } from "@material-ui/data-grid";
-import Address from "./Address";
-import ShareComponent from "./Refer";
-import { useEffect } from "react";
 import { toast } from "react-hot-toast";
-import Credit from "./Credit";
-import Order from "./Order";
-// import Address from "./Address";
+import styles from "./styles";
+import { FaEdit } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+
 const ProfileContent = ({ active }) => {
   const { user, error, successMessage } = useSelector((state) => state.profile);
   const [name, setName] = useState(user && user.name);
@@ -19,7 +13,8 @@ const ProfileContent = ({ active }) => {
   const [phoneNumber, setPhoneNumber] = useState(user && user.contactNumber);
   const [password, setPassword] = useState("");
   const [avatar, setAvatar] = useState(null);
-  
+  const [isEditing, setIsEditing] = useState(false);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -31,7 +26,7 @@ const ProfileContent = ({ active }) => {
       toast.success(successMessage);
       dispatch({ type: "clearMessages" });
     }
-  }, [error, successMessage]);
+  }, [error, successMessage, dispatch]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -51,146 +46,82 @@ const ProfileContent = ({ active }) => {
   };
 
   return (
-    <div className="w-full ">
-      {/* profile */}
-      {active === 1 && (
-        <div className=" h-full ">
-          <div className="flex justify-center w-full mt-[55px] ">
-            <div className="relative">
-              <img
-                src={`${user?.image}`}
-                className="w-[150px] h-[150px] rounded-full object-cover border-[3px] border-[#3ad132]"
-                alt=""
-              />
-              <div className="w-[30px] h-[30px] bg-[#E3E9EE] rounded-full flex items-center justify-center cursor-pointer absolute bottom-[5px] right-[5px]">
-                <input
-                  type="file"
-                  id="image"
-                  className="hidden"
-                  onChange={handleImage}
-                />
-                <label htmlFor="image">
-                  <AiOutlineCamera />
-                </label>
-              </div>
-            </div>
-          </div>
-          <br />
-          <br />
-          <div className="w-full px-5">
-            <form onSubmit={handleSubmit} aria-required={true}>
-              <div className="w-full 800px:flex block pb-3">
-                <div className=" w-[100%] 800px:w-[50%]">
-                  <label className="block pb-2">Full Name</label>
-                  <input
-                    type="text"
-                    className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
-                    required
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
-                <div className=" w-[100%] 800px:w-[50%]">
-                  <label className="block pb-2">Email Address</label>
-                  <input
-                    type="text"
-                    className={`${styles.input} !w-[95%] mb-1 800px:mb-0`}
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-              </div>
-
-              <div className="w-full 800px:flex block pb-3">
-                <div className=" w-[100%] 800px:w-[50%]">
-                  <label className="block pb-2">Phone Number</label>
-                  <input
-                    type="number"
-                    className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
-                    required
-                    value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                  />
-                </div>
-
-                <div className=" w-[100%] 800px:w-[50%]">
-                  <label className="block pb-2">Enter your password</label>
-                  <input
-                    type="password"
-                    className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-              </div>
-              <input
-                className={`w-[250px] h-[40px] border border-[#3a24db] text-center text-[#3a24db] rounded-[3px] mt-8 cursor-pointer mb-20`}
-                required
-                value="Update"
-                type="submit"
-              />
-            </form>
+    <div className="grid w-11/12 mx-auto lg:mr-28 -ml-0 overflow-x-hidden gap-20">
+      <div className="flex justify-center w-full mt-14">
+        <div className="relative">
+          <img
+            src={`${user?.image}`}
+            className="w-[150px] h-[150px] rounded-full object-cover border-4 border-green-500"
+            alt="Profile"
+          />
+          <div className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center cursor-pointer absolute bottom-1 right-1">
+            <input
+              type="file"
+              id="image"
+              className="hidden"
+              onChange={handleImage}
+            />
+            <label htmlFor="image">
+              <AiOutlineCamera />
+            </label>
           </div>
         </div>
-      )}
-
-      {/* order */}
-      {active === 2 && (
-        <div className="mt-[4px]">
-          {/* <AllOrders /> */}
-          <Order />
-
+      </div>
+      <form onSubmit={handleSubmit} aria-required={true} className="w-full">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5">
+          <button
+            onClick={() => setIsEditing(!isEditing)}
+            className="absolute top-52 right-6 lg:top-48 lg:right-20 m-4 bg-tr"
+          >
+            {isEditing ? <ImCross size={25} /> : <FaEdit size={25} />}
+          </button>
+          <div className="w-full">
+            <label className="block pb-2">Full Name</label>
+            <input
+              type="text"
+              className={`${styles.input} w-full h-14 p-3 text-sm`}
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              disabled={!isEditing}
+            />
+          </div>
+          <div className="w-full">
+            <label className="block pb-2">Email Address</label>
+            <input
+              type="email"
+              className={`${styles.input} w-full h-14 p-3 text-sm`}
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              disabled={!isEditing}
+            />
+          </div>
+          <div className="w-full">
+            <label className="block pb-2">Phone Number</label>
+            <input
+              type="tel"
+              className={`${styles.input} w-full h-14 p-3 text-sm`}
+              required
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              disabled={!isEditing}
+            />
+          </div>
         </div>
-      )}
-
-      {/* Refund */}
-      {active === 3 && (
-        <div className=" mt-[4px]  ">
-          <Address />
-
-        </div>
-      )}
-
-
-      {active === 4 && (
-        <div className=" mt-[4px]  ">
-          <Credit />
-          
-
-        </div>
-      )}
-
-      {/* Track order */}
-      {active === 5 && (
-        <div>
-          {/* <TrackOrder /> */}
-          trackorder
-        </div>
-      )}
-
-      {/* Change Password */}
-      {active === 6 && (
-        <div>
-       <ShareComponent />
-        </div>
-      )}
-
-      {/*  user Address */}
-      {active === 7 && (
-        <div>
-          {/* <Address /> */}
-        </div>
-      )}
+        {isEditing && (
+          <div className="flex ">
+            <input
+              className="w-64 h-10 border border-blue-500 text-center text-blue-500 rounded-md mt-8 cursor-pointer mb-20"
+              value="Update"
+              type="submit"
+              disabled={!isEditing}
+            />
+          </div>
+        )}
+      </form>
     </div>
   );
 };
-
-
-
-
-
-
 
 export default ProfileContent;
---
+++ client/src/pages/Refer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { FaFacebook, FaWhatsapp, FaCopy } from "react-icons/fa";
+import { useSelector } from "react-redux";
+
+function Refer() {
+  const { user } = useSelector((state) => state.profile);
+  const link = `https://absencemain.vercel.app/login?refer=${user.referralCode}`;
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(link)
+      .then(() => {
+        alert("Link copied to clipboard!");
+      })
+      .catch((error) => {
+        console.error("Failed to copy:", error);
+      });
+  };
+
+  const shareOnFacebook = () => {
+    const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+      link
+    )}`;
+    window.open(url, "_blank");
+  };
+
+  const shareOnWhatsApp = () => {
+    const url = `https://api.whatsapp.com/send?text=${encodeURIComponent(
+      link
+    )}`;
+    window.open(url, "_blank");
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center space-y-4 md:space-y-0 md:flex-row md:justify-center md:items-center mt-[130px] gap-11">
+      <button
+        onClick={shareOnFacebook}
+        className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded flex items-center"
+      >
+        <FaFacebook className="mr-2" /> Share on Facebook
+      </button>
+      <button
+        onClick={shareOnWhatsApp}
+        className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded flex items-center"
+      >
+        <FaWhatsapp className="mr-2" /> Share on WhatsApp
+      </button>
+      <button
+        onClick={copyToClipboard}
+        className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded flex items-center"
+      >
+        <FaCopy className="mr-2" /> Copy Link
+      </button>
+    </div>
+  );
+}
+
+export default Refer;
