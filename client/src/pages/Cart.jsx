e95a5bef5509f39489b9856f99fea8b35fd0581e
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-07-07 01:22:54 +0530

    .

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -49,7 +49,7 @@ function Cart() {
           <motion.div
             id="cart"
             ref={ref}
-            className="fixed top-0 right-0 bottom-0 lg:w-[450px] w-[320px] bg-white p-4 z-50 max-h-screen overflow-auto "
+            className="fixed top-0 right-0 bottom-0 lg:w-[450px] w-[320px] bg-white p-4 z-50 max-h-screen overflow-hidden "
             initial={{ x: "100%", opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             exit={{ x: "100%", opacity: 0 }}
@@ -73,13 +73,13 @@ function Cart() {
                 />
               ) : (
                 <div className="flex w-full justify-between flex-col h-screen">
-                  <div className="w-full bg-bg-color-2 max-h-[400px] py-4 overflow-x-hidden overflow-y-auto scrollbar-w-[0.35vw]">
+                  <div className="w-full bg-bg-color-2 h-[400px] py-4 overflow-x-hidden overflow-y-scroll scrollbar-w-[0.35vw]">
                     {cart.map((item, ind) => (
                       <CartItems key={ind} {...item} />
                     ))}
                   </div>
 
-                  <div className="w-full  mb-[130px] lg:mb-[5rem] sm:mb-[5rem] md:mb-[5rem] text-gray-500 font-montserrat">
+                  <div className="w-full  mb-[180px] lg:mb-[5rem] sm:mb-[5rem] md:mb-[5rem] text-gray-500 font-montserrat">
                     <div className="flex flex-col">
                       <h3 className="font-montserrat text-sm font-medium">
                         Total &nbsp; ({cartQuantity}{" "}
